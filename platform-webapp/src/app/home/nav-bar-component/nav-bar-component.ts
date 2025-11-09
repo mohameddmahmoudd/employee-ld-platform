d55ee4b9c69b8b5ef37860cbff748228cdde546a
MariamH78 <mariamhahamid@gmail.com>
2025-11-09 17:33:10 +0200

    added responsive notifications dialog

--- platform-webapp/src/app/home/nav-bar-component/nav-bar-component.ts
+++ platform-webapp/src/app/home/nav-bar-component/nav-bar-component.ts
@@ -2,6 +2,7 @@ import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -9,6 +10,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Router, RouterModule, RouterOutlet } from "@angular/router";
+import { NotificationsDialog } from '../notifications/notifications-dialog';
 
 @Component({
   selector: 'app-nav-bar-component',
@@ -47,13 +49,42 @@ export class NavBarComponent {
     const media = inject(MediaMatcher);
     this._mobileQuery = media.matchMedia('(max-width: 600px)');
     this.isMobile.set(this._mobileQuery.matches);
-    this._mobileQueryListener = () =>
+    this._mobileQueryListener = () => {
       this.isMobile.set(this._mobileQuery.matches);
+      this.dialog.closeAll();
+    }
     this._mobileQuery.addEventListener('change', this._mobileQueryListener);
   }
 
   ngOnDestroy(): void {
     this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog(event: MouseEvent) {
+    const buttonRect = (event.currentTarget as HTMLElement).getBoundingClientRect();
+
+    if (this.isMobile()) {
+      // width: `100vw`,
+      this.dialog.open(NotificationsDialog, {
+        width: `100vw`,
+        position: {
+          top: `${buttonRect.bottom + 10}px`,
+        },
+      });
+    } else {
+      this.dialog.open(NotificationsDialog, {
+        position: {
+          top: `${buttonRect.bottom + 10}px`,
+          left: `${buttonRect.left - 28 - 10 + buttonRect.width / 2}px`, //28 is caret position, 10 is half caret width
+        },
+        // backdropClass: "backdrop" 
+        // TODO: decide whether we want outline to notifs and no backdrop or we want backdrop?
+
+      });
+    }
+
   }
   // activeLink = this.links[0];
 }
